c4b46a7b4942d3ede839d4e024da27e682a63bef
Ale <ale@incrowd.ws>
2014-05-10 12:15:13 +0400

    Binary BWT! :metal:

--- src/main/java/ws/incrowd/ale/school/archiver/BWT.java
+++ src/main/java/ws/incrowd/ale/school/archiver/BWT.java
@@ -1,62 +1,84 @@
 package ws.incrowd.ale.school.archiver;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class BWT {
-  public static void main(String[] args) {
-    System.out.println(encode(args[0], '~'));
-    System.out.println(decode(args[0], '~'));
-  }
+  public static byte[] encode(byte[] str, byte padding) {
+    byte[] src = new byte[str.length + 1];
+    System.arraycopy(str, 0, src, 0, str.length);
+    src[str.length] = padding;
 
-  public static String encode(String str, char padding) {
-    String[] perms = permutations(str + padding);
-    Arrays.sort(perms);
-    char[] lastChars = new char[perms.length];
+    byte[][] perms = permutations(src);
+    Byte[][] pErms = new Byte[perms.length][perms[0].length];
+
     for (int i = 0; i < perms.length; i++) {
-      lastChars[i] = perms[i].charAt(perms[i].length()-1);
-    }
-
-    return arrayToString(lastChars);
-  }
-
-  public static String decode(String str, char padding) {
-    String[] perms = new String[str.length()];
-
-    for (int j = 0; j < str.length(); j++) {
-      perms[j] = "";
-    }
-
-    for (int i = 0; i < str.length(); i++) {
-      for (int j = 0; j < str.length(); j++) {
-        perms[j] = str.charAt(j) + perms[j];
-      }
-      Arrays.sort(perms);
-    }
-
-    for (int j = 0; j < str.length(); j++) {
-      if (perms[j].charAt(perms[j].length() - 1) == padding) {
-        return perms[j].substring(0, str.length()-1);
+      for (int j = 0; j < perms[0].length; j++) {
+        pErms[i][j] = (Byte) perms[i][j];
       }
     }
 
-    return perms[0].substring(0, str.length()-1);
+    Arrays.sort(pErms, TwoDComparator);
+    byte[] lastChars = new byte[pErms.length];
+    for (int i = 0; i < pErms.length; i++) {
+      lastChars[i] = pErms[i][pErms[i].length-1];
+    }
+
+    return lastChars;
   }
 
-  private static String[] permutations(String orig) {
-    String[] perms = new String[orig.length()];
-    String now = new String(orig);
-    for (int i = 0; i < orig.length(); i++) {
-      perms[i] = now;
-      now = now.substring(1) + now.charAt(0);
+  public static byte[] decode(byte[] data, byte padding) {
+    Byte[][] perms = new Byte[data.length][data.length];
+    for (int i = 0; i < data.length; i++) {
+      for (int j = 0; j < data.length; j++) {
+        System.arraycopy(perms[j], 0, perms[j], 1, i);
+        perms[j][0] = (Byte) data[j];
+      }
+      Arrays.sort(perms, TwoDComparator);
+    }
+
+    for (int i = 0; i < data.length; i++) {
+      if (perms[i][perms[i].length - 1] == padding) {
+        byte[] res = new byte[perms[i].length - 1];
+        // System.arraycopy(perms[i], 0, res, 0, res.length);
+        for (int j = 0; j < res.length; j++) {
+          res[j] = perms[i][j].byteValue();
+        }
+        return res;
+      }
+    }
+    return null;
+  }
+
+  private static Comparator<Byte[]> TwoDComparator = new Comparator<Byte[]>() {
+    public int compare(Byte[] b1, Byte[] b2) {
+      int minLength = (b1.length < b2.length) ? b1.length : b2.length;
+      for (int i = 0; i < minLength; i++) {
+        int res = b1[i].compareTo(b2[i]);
+        if (res != 0) {
+          return res;
+        }
+      }
+      if (b1.length == b2.length) {
+        return 0;
+      } else {
+        return (b1.length < b2.length) ? 1 : -1;
+      }
+    }
+  };
+
+  private static byte[][] permutations(byte[] orig) {
+    byte[][] perms = new byte[orig.length][orig.length];
+    
+    byte[] now = orig;
+    for (int i = 0; i < orig.length; i++) {
+      perms[i] = now.clone();
+      byte tmp = now[0];
+      for (int j=0; j < now.length - 1; j++) {
+        now[j] = now[j+1];
+      }
+      now[now.length - 1] = tmp;
     }
     return perms;
-  }
-
-  private static String arrayToString(char[] chars) {
-    String string = "";
-    for (char c : chars) {
-      string = string + c;
-    }
-    return string;
   }
 }
--- src/test/java/ws/ale/incrowd/school/archiver/BWTTest.java
+++ src/test/java/ws/ale/incrowd/school/archiver/BWTTest.java
@@ -3,6 +3,8 @@ package ws.incrowd.ale.school.archiver;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+import java.util.Arrays;
 
 public class BWTTest extends TestCase {
   public BWTTest(String testName) {
@@ -16,9 +18,25 @@ public class BWTTest extends TestCase {
   /**
    * Basic BWT test.
    */
+  // public void testBWT() {
+  //   assertEquals(
+  //     BWT.decode(BWT.encode("The quick brown fox jumps over the lazy doge.", '~'), '~'),
+  //     "The quick brown fox jumps over the lazy doge.");
+  // }
+
   public void testBWT() {
-    assertEquals(
-      "The quick brown fox jumps over the lazy doge.",
-      BWT.decode(BWT.encode("The quick brown fox jumps over the lazy doge.", '~'), '~'));
+    byte[] arr = {1, 2, 3, 4};
+    byte[] exp = {4, -1, 1, 2, 3};
+    assertArrayEquals(
+      BWT.encode(arr, (byte) -1),
+      exp);
+  }
+
+  public void testUnBWT() {
+    byte[] arr = {4, -1, 1, 2, 3};
+    byte[] exp = {1, 2, 3, 4};
+    assertArrayEquals(
+      BWT.decode(arr, (byte) -1),
+      exp);
   }
 }
